4824f1de3879ea267e03dec7ecb070afeb6bfb03
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-02-24 16:59:55 -0300

    feat: implement Postgres database driver
    Add Postgres class extending Database and implementing IDisposable.

---
+++ src/services/Postgres.cs
@@ -0,0 +1,40 @@
+﻿using FieldTeamEquipmentInventory.Interfaces;
+using FieldTeamEquipmentInventory.Services;
+using Npgsql;
+
+namespace FieldTeamEquipmentInventory.Services;
+
+public class Postgres : Database, IDatabase
+{
+    private bool _disposed = false;
+    public Postgres() : base(
+        new Npgsql.NpgsqlConnection(
+            System.Environment.GetEnvironmentVariable("DNS_STRING")))
+    {
+        base.connection.Open();
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposed)
+        {
+            if (disposing)
+            {
+                // Dispose managed resources here.
+                base.connection?.Dispose();
+            }
+            // Dispose unmanaged resources here.
+            _disposed = true;
+        }
+    }
+    ~Postgres()
+    {
+        // Destructor (optional)
+        Dispose(false);
+    }
+}
