ee3cd7d2230693534f008fe9630fb39d58ea5737
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-02-24 09:55:19 -0300

    feat: add Transaction model

---
+++ src/models/Transaction.cs
@@ -0,0 +1,35 @@
+﻿using FieldTeamEquipmentInventory.Helpers;
+
+namespace FieldTeamEquipmentInventory.Models;
+
+public class Transaction
+{
+    public DateTime Timestamp { get; set; }
+    public long IdEquipment { get; set; }
+    public KindEnum Kind { get; set; } = KindEnum.Idle;
+    public int IdEmployerFrom { get; set; }
+    public int IdEmployerTo { get; set; }
+    public string? Note { get; set; }
+    public Equipment Equipment { get; set; }
+    public enum KindEnum { Idle = 0, Checkin = 1, Checkout = 2, ToRepair = 3, FromRepair = 4, Removed = 5 }
+
+    public Transaction
+    (
+        DateTime timestamp,
+        long idEquipment,
+        KindEnum kind,
+        int idEmployerFrom,
+        int idEmployerTo,
+        Equipment equipment,
+        string? note = null
+    )
+    {
+        Timestamp = timestamp;
+        IdEquipment = idEquipment;
+        Kind = kind;
+        IdEmployerFrom = idEmployerFrom;
+        IdEmployerTo = idEmployerTo;
+        Equipment = equipment;
+        Note = note;
+    }
+}
