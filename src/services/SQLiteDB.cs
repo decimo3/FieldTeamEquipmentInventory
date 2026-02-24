74d68d065f57773aba76c88174b8a80e26884762
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-02-24 16:59:22 -0300

    feat: implement SQLite database driver
    Add SQLiteDB class extending Database and implementing IDisposable.

---
+++ src/services/SQLiteDB.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Data.Sqlite;
+
+using FieldTeamEquipmentInventory.Interfaces;
+
+namespace FieldTeamEquipmentInventory.Services;
+
+public sealed class SQLiteDB : Database, IDatabase
+{
+    private bool _disposed = false;
+    public SQLiteDB() : base(
+        new SqliteConnection(
+            System.Environment.GetEnvironmentVariable("DNS_STRING")))
+    {
+        base.connection.Open();
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposed)
+        {
+            if (disposing)
+            {
+                // Dispose managed resources here.
+                base.connection?.Dispose();
+            }
+            // Dispose unmanaged resources here.
+            _disposed = true;
+        }
+    }
+    ~SQLiteDB()
+    {
+        // Destructor (optional)
+        Dispose(false);
+    }
+
+}
