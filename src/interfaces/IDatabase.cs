ea80ff9630970be2cc08ab49fd2ea44bc861a73f
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-02-24 12:49:49 -0300

    feat: add IDatabase interface
    Defines the contract for all required database access operations.

---
+++ src/interfaces/IDatabase.cs
@@ -0,0 +1,16 @@
+﻿using FieldTeamEquipmentInventory.Models;
+
+namespace FieldTeamEquipmentInventory.Interfaces;
+
+public interface IDatabase
+{
+    public void AddEmployer(Employer employer);
+    public Employer? GetEmployer(int id);
+    public List<Employer> GetEmployers();
+    public void AddEquipment(Equipment equipment);
+    public Equipment? GetEquipment(long id);
+    public List<Equipment> GetEquipments();
+    public void AddTransaction(Transaction transaction);
+    public Transaction? GetTransaction(long id);
+    public List<Transaction> GetTransactions();
+}
