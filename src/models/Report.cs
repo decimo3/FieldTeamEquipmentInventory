591db0d75c64d696f8f1083d091c2f4b99faa102
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-02-25 09:50:09 -0300

    feat: add Report model

---
+++ src/models/Report.cs
@@ -0,0 +1,15 @@
+﻿namespace FieldTeamEquipmentInventory.Models;
+
+public class Report
+{
+    public String CreateAt { get; set; }
+    public Transaction.KindEnum TransactionKind { get; set; }
+    public long IdEquipment { get; set; }
+    public Equipment.KindEnum EquipmentKind { get; set; }
+    public Equipment.StatusEnum EquipmentStatus { get; set; }
+    public string Kit { get; set; }
+    public int IdEmployerFrom { get; set; }
+    public int IdEmployerTo { get; set; }
+    public string? Note { get; set; }
+
+}
