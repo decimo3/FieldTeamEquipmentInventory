16da6f7e91ca9968e6f4009d407e931054bf60e1
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-02-25 09:06:17 -0300

    refactor: navigate to MainScreen via DI container
    Register MainScreen as a service to propagate dependency injection.

--- src/MainWindow.cs
+++ src/MainWindow.cs
@@ -1,4 +1,9 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Extensions.DependencyInjection;
+
+using FieldTeamEquipmentInventory.Screens;
 
 namespace FieldTeamEquipmentInventory;
 
@@ -6,5 +11,16 @@ public partial class MainWindow : Window
 {
     public MainWindow()
     {
+        Title = Helpers.Resources.GetString("APPLICATION_TITLE");
+        Width = 800;
+        Height = 600;
+        var frame = new Frame
+        {
+            NavigationUIVisibility = NavigationUIVisibility.Hidden
+        };
+        Content = frame;
+        // TODO - change to AuthScreen when implemented
+        var page = MainApplication.Host!.Services.GetRequiredService<MainScreen>();
+        frame.Navigate(page);
     }
 }
