ae0b6d469338f02491275d51e55f967356782878
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-02-25 09:25:21 -0300

    feat: add Hodor helper class
    Class used to store employers registry as global variables

---
+++ src/helpers/Hodor.cs
@@ -0,0 +1,10 @@
+﻿namespace FieldTeamEquipmentInventory.Helpers;
+
+/// <summary>
+/// Class used to store employers registry as global variables
+/// </summary>
+public static class Hodor
+{
+    public static int From { get; set; } = 0;
+    public static int To { get; set; } = 0;
+}
