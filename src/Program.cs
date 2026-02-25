042f943af3a8944a174020cc782e67a1ea141626
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-02-25 09:55:43 -0300

    feat: load dev.env file in debug environment
    Automatically load development variables during debug builds.

--- src/Program.cs
+++ src/Program.cs
@@ -5,6 +5,11 @@ public static class Program
     [STAThread]
     public static void Main()
     {
+        #if DEBUG
+            dotenv.net.DotEnv.Load(
+                new dotenv.net.DotEnvOptions(
+                    envFilePaths: ["dev.env"]));
+        #endif
         var app = new MainApplication();
         var window = new MainWindow(); 
         app.Run(window);
