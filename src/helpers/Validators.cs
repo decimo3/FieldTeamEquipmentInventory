576590d3832ebbcd69ee89f9ea5e99ee65d9042a
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-02-25 09:13:09 -0300

    feat: add Validators helper class
    Provide static methods to restrict TextBox input to numeric or alphabetic characters.

---
+++ src/helpers/Validators.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+namespace FieldTeamEquipmentInventory.Helpers;
+
+public static class Validators
+{
+    private readonly static Regex TxtOnly = new("[^A-z ]");
+    private readonly static Regex NumOnly = new("[^0-9]");
+    public static void NumberInputOnly(object sender, System.Windows.Input.TextCompositionEventArgs e) => e.Handled = NumOnly.IsMatch(e.Text);
+    public static void CharacterInputOnly(object sender, System.Windows.Input.TextCompositionEventArgs e) => e.Handled = TxtOnly.IsMatch(e.Text);
+}
