dd3b87d59d84090485c546839e9cb371c79eac46
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-02-25 09:09:35 -0300

    feat: add BitmapLoader helper class
    Provide conversion from byte array to BitmapImage.

---
+++ src/helpers/BitmapLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace FieldTeamEquipmentInventory.Helpers;
+
+public static class BitmapLoader
+{
+    public static BitmapImage? LoadBitmap(byte[] bytes)
+    {
+        try
+        {
+            var bitmap = new BitmapImage();
+            using var ms = new System.IO.MemoryStream(bytes);
+            bitmap.BeginInit();
+            bitmap.CacheOption = BitmapCacheOption.OnLoad;
+            bitmap.StreamSource = ms;
+            bitmap.EndInit();
+            bitmap.Freeze();
+            return bitmap;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}
